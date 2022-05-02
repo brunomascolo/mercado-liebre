46fdc2913c52d806f22813f8b99959b6a5e4e42d
brunomascolo <brunomascolo95@gmail.com>
2022-05-01 23:29:43 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath)); //esta linea le dice a express que queremos tener a la carpeta public como un recurso de archivos estaticos para que se puedan consumit 
+//de manera sencilla
+
+app.listen(3000, () => 
+console.log("Servidor corriendo en el puerto 3000"));
+
+app.get('/', (req,res) => {
+    let viewsPath = path.resolve(__dirname, "./views/home.html");
+    res.sendFile(viewsPath);
+})
